e5fa32284b18644161491457dc6907c9a3ad6166
Meredith <mcourtney02124@gmail.com>
2018-03-18 08:49:31 -0400

    Revert "revert the addition of sipp files, add import of pysipp"
    This reverts commit 233c96cfc395affa7eb12a5de6b9add63debe36f.
    
    t

--- src/index.py
+++ src/index.py
@@ -12,7 +12,6 @@ import datetime as dt
 import logging
 import time
 from flask import Flask, jsonify, request
-import pysipp
 
 from src.model.transaction import Transaction, TransactionSchema
 from src.model.expense import Expense, ExpenseSchema
